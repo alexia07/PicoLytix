7d4dfac0497b80274b50df29caa8100135d10ee6
Alexia Vernier <alexiavernier@gmail.com>
2026-08-06 20:43:12 +0200

    [init] add board & config folders

---
+++ board/pico2.h
@@ -0,0 +1,32 @@
+/**
+ * @file pico2.h
+ * @brief Pin definitions for the Raspberry Pi Pico 2 board.
+ */
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for
+// information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS 17
+#define PIN_SCK 18
+#define PIN_MOSI 19
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for
+// information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for
+// information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
---
+++ config/conf_board.h
@@ -0,0 +1,6 @@
+/**
+ * @file conf_board.h
+ * @brief Board configuration header file.
+ */
+
+#define UART_BAUD_RATE 115200
