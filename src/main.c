e713f393cfbafb93db1ead581a2dc5117be79870
Alexia Vernier <alexiavernier@gmail.com>
2026-08-18 16:51:11 +0200

    [init] basic interpolation module

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include "hardware/clocks.h"
 #include "hardware/i2c.h"
-#include "hardware/interp.h"
 #include "hardware/pio.h"
 #include "hardware/spi.h"
 #include "hardware/timer.h"
@@ -27,18 +26,6 @@ int main() {
 #else
   blink_pin_forever(0, 6, 1);
 #endif
-  // For more pio examples see
-  // https://github.com/raspberrypi/pico-examples/tree/master/pio
-
-  // Interpolator example code
-  // interp_config cfg = interp_default_config();
-  // Now use the various interpolator library functions for your use case
-  // e.g. interp_config_clamp(&cfg, true);
-  //      interp_config_shift(&cfg, 2);
-  // Then set the config
-  // interp_set_config(interp0, 0, &cfg);
-  // For examples of interpolator use see
-  // https://github.com/raspberrypi/pico-examples/tree/master/interp
 
   // Timer example code - This example fires off the callback after 2000ms
   // add_alarm_in_ms(2000, alarm_callback, NULL, false);
---
+++ src/modules/m_interp.h
@@ -0,0 +1,9 @@
+/**
+ * @file m_interp.h
+ * @brief Header file for the Interpolator module.
+ */
+
+/**
+ * @brief Initializes the Interpolator module and configures it for use.
+ */
+void m_interp_init(void);
