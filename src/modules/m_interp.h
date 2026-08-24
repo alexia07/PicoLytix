6b5c8627380e35ed532ee7aa36964b4a8ccedcde
Alexia Vernier <alexiavernier@gmail.com>
2026-08-24 19:10:49 +0200

    [fix] add include guards

--- src/modules/m_interp.h
+++ src/modules/m_interp.h
@@ -3,7 +3,12 @@
  * @brief Header file for the Interpolator module.
  */
 
+#ifndef M_INTERP_H
+#define M_INTERP_H
+
 /**
  * @brief Initializes the Interpolator module and configures it for use.
  */
 void m_interp_init(void);
+
+#endif // M_INTERP_H
